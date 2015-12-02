9529c8aab8b61c98d7a5e7379d6f36f5e674321c
johnathan <johnathan@kodokuman.com>
2015-12-02 14:28:41 +0900

    start_all fixed

--- psinfo.sh
+++ psinfo.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+echo "ps aux | grep -v grep | grep "${1}" >> /tmp/ps1.txt"
 while [ 1 ]
 do
   ps aux | grep -v grep | grep "${1}" >> /tmp/ps1.txt
-    paste /tmp/ps1.txt
-    sleep 5
+  paste /tmp/ps1.txt
+  sleep 5
 done >> /tmp/psinfo.dat
